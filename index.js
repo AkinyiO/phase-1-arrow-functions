b338080519899aa233443b873d78b2a8ac4c78c5
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-06-09 23:42:35 +0300

    Akinyi's Arrow Functions

---
+++ index.js
@@ -0,0 +1,11 @@
+//const divide = () => { }
+
+const divide = (divide1, divide2) => 2000 / 100;
+
+const square = x => x * x;
+    let x = 2;
+
+const add = (a, b) => a + b; {
+    let a = 3;
+    let b = 4;
+}
